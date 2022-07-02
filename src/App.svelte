8cfbfe50f098fd2a44da63dcb51bffe7629aa0ad
Reuben Menezes <reubenmenezes1@gmail.com>
2022-07-02 22:14:29 +0530

    Make todo component and form within

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
-<style>
-  h1{
-    color:#333;
-  }
-</style>
-<h1>To-do list made with Svelte</h1>
+<script>
+  import Todos from './components/Todos.svelte'
+</script>
+
+<Todos />
---
+++ src/components/Todos.svelte
@@ -0,0 +1,17 @@
+<script>
+    let todo = [];
+    $:totalTodos = todo.length;
+    let completedTodos;
+    function addTodo(event){
+        console.log(event.target)
+    }
+</script>
+
+<div class="todo">
+    <h2>What are we doing today?</h2>
+    <h3>You have completed {completedTodos} out of {totalTodos}</h3>
+    <form on:submit|preventDefault = {addTodo}>
+        <input type="text" placeholder="What are we doing today?">
+        <button type="submit">Add Todo</button>
+    </form>
+</div>
